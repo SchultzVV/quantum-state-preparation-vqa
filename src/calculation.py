b837a9dc00e745eda0d1dbd9562d7c44acfbcaa1
v <xultezz@gmail.com>
2023-07-19 15:41:57 -0300

    .

--- src/calculation.py
+++ src/calculation.py
@@ -70,7 +70,7 @@ class Calculate(object):
         pass
 
    
-    def phase_flip():
+    def phase_flip(J):
         def K_0(J):
             return sqrt(1-J/2)*Pauli(0)
         def K_1(J):
@@ -78,7 +78,12 @@ class Calculate(object):
         print(K_0, K_1)
         return K_0, K_1
     
-    def RHO_t_NM(state,J):
+    def RHO_t_Ana(state,J):
+        tp1 = TP(K_0(J),K_1(J))
+        tp2 = TP(K_1(J),K_0(J))
+        return tp1*proj(state)*tp1.T + tp2*proj(state)*tp2.T
+
+    def RHO_t_Ana(state,J):
         tp1 = TP(K_0(J),K_1(J))
         tp2 = TP(K_1(J),K_0(J))
         return tp1*proj(state)*tp1.T + tp2*proj(state)*tp2.T
